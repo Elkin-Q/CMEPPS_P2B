df4ffeb748be9c6703368b658a3a299e8d4649fe
Elkin <Elkin@DESKTOP-SDOEEVF>
2024-10-17 23:17:25 +0200

    Commit 1

---
+++ src/paquete/Empleado.java
@@ -0,0 +1,55 @@
+package paquete;
+
+public class Empleado {
+public enum TipoEmpleado{Vendedor, Encargado};
+	
+	public static float CalculaNominaBruta(TipoEmpleado tipo, float ventasMes, float HorasExtra) {
+		
+		float SalarioBase=0, primas=0, extras=0;
+		
+		//calculo salario base
+		if(tipo==TipoEmpleado.Encargado) {
+			SalarioBase=2500;
+		}else if(tipo==TipoEmpleado.Vendedor) {
+			SalarioBase=2000;
+		}
+		
+		//calculo prima	
+		if(ventasMes>=1500) {
+			primas=200;
+		}else if(ventasMes>=1000) {
+			primas=100;
+		}else if(ventasMes<0) {
+			primas=-1;
+		}
+			
+		//calculo horas extras
+		
+		if(HorasExtra<0) {
+			extras=-1;
+		}else {
+			extras=30*HorasExtra;
+		}
+		
+		//Si los parametros introducidos son erroneos devolver -1	
+		if(primas==-1 || extras==-1) {
+			return -1;
+		}else {
+			float salario=SalarioBase+primas+extras;
+			return salario;
+		}	
+	}
+	
+	
+	public static float calculoNominaNeta(float nominaBruta) {
+		float nominaNeta=2;
+		if(nominaBruta<2100) {
+			nominaNeta=nominaBruta;
+		}else if(nominaBruta>2100 && nominaBruta<2500){
+			nominaNeta=nominaBruta-nominaBruta*0.15f;
+		}else if(nominaBruta>2500) {
+			nominaNeta=nominaBruta-nominaBruta*0.18f;
+		}
+		return nominaNeta;
+	}
+}
